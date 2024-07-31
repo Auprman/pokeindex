2bad33c43748ac313abdce99ed38912d0cadc382
Felix Oppermann <felixoppermann@freenet.de>
2024-07-31 21:58:10 +0200

    updated modal

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon/"
 const URL_PICTURE = "https://img.pokemondb.net/artwork/vector/large/"
 
+let modal = document.getElementById('modal');
+
+
+addPokemon()
+
 function rotatePokeball() {
     document.querySelector("#logoPic").style.rotate = "360deg"
 }
+
 
 async function getData(path="") {
     let response = await fetch(URL_POKEMON)
     return await response.json();
 }
 
+
 async function displayData() {
     data = await getData();
     return data
 }
+
 
 function addDataToContainer(data) {
     let container = document.getElementById('content');
@@ -38,8 +46,8 @@ async function addPokemon() {
 
 function htmlContent(element, index) {
     return `
-         <div class = "pokemon-container">
-            <div class = "pokemon-card">
+         <div id="${element['name']}" class = "pokemon-container">
+            <div onclick="showModal(${element["name"].toString()})" class = "pokemon-card">
                 <h2>${element['name']}</h2>
                 <img src="${URL_PICTURE + element['name'] + ".png"}">
             </div>
@@ -47,23 +55,39 @@ function htmlContent(element, index) {
          `
 }
 
-addPokemon()
+function modalContent() {
+    return `
+    <div id="modal-content">
+        <h2>Pikatchu</h2>
+        <img src="https://img.pokemondb.net/artwork/vector/large/pikachu.png" alt="Pokemon Picture" >
+        <div id="attributes-pokemon">
+
+        </div>
+    </div>
+    `
+}
 
 
-function closeModal(event) {
-    let modal = document.getElementById('modal');
+function showModal(name) {
+   modal.classList.remove('hidden')
+   modal.classList.add('visible')
+   updateContent(name.id);
+}
+
+
+function hideModal() {
     modal.classList.remove('visible');
     modal.classList.add('hidden');
 }
 
 
-
-
-
+function updateContent(name) {
+    document.getElementById('modalPokemonName').innerText = name;
+    document.getElementById('modalPokemonImg').src = URL_PICTURE + name + ".png";
+}
 
 // Hiermit den LoadScreeb basteln
 document.addEventListener('DOMContentLoaded' ,() =>{
-    let modal = document.getElementById('modal');
     modal.addEventListener('click',(e) => {
         e.stopPropagation();
     })
