edcc235a04dd8b218993e067dc9a944358a04db3
Felix Oppermann <felixoppermann@freenet.de>
2024-08-03 23:09:07 +0200

    modal fixed onclick and some css changes

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 
-let displayAmount = 20;
+let displayAmount = 40;
+
 const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon?limit="+ displayAmount +"&offset=0";
 const URL_START = "https://pokeapi.co/api/v2/";
-const URL_COLOR = "https://pokeapi.co/api/v2/pokemon-color/"
+const URL_COLOR = "https://pokeapi.co/api/v2/pokemon/"
 const URL_PICTURE = "https://img.pokemondb.net/artwork/vector/large/";
 
 let modal = document.getElementById('modal');
 
 
-addPokemon()
+
+
 
 function rotatePokeball() {
     document.querySelector("#logoPic").style.rotate = "360deg"
@@ -22,12 +24,18 @@ async function getData(path) {
 }
 
 
-function addDataToContainer(data) {
+async function addDataToContainer(name,path) {
     let container = document.getElementById('content');
     
-    data.results.forEach((element, index) => {
+    name.results.forEach(async (element, index) => {
+        const pokename = element['name']
+        const pokemonData = await getData(URL_COLOR + pokename); 
+        container.innerHTML += htmlContent(element, index);
+        const card = document.getElementById(pokename);
         
-         container.innerHTML += htmlContent(element, index);
+        
+        card.classList.add(pokemonData['types'][0]['type']['name'])
+
     });
    
 }
@@ -36,7 +44,9 @@ function addDataToContainer(data) {
 async function addPokemon() {
     try {
         pokemon = await getData(URL_POKEMON);
-        addDataToContainer(pokemon)
+        path = await getData(URL_COLOR);
+        
+        addDataToContainer(pokemon,path )
     } catch (error) {
         console.error(error)
     }
@@ -44,24 +54,32 @@ async function addPokemon() {
 
 
 function htmlContent(element, index) {
+    let name = element['name'].toString();
     return `
-         <div id="${element['name']}" class = "pokemon-container">
-            <div onclick="showModal(${element["name"].toString()})" class = "pokemon-card">
-                <h2>${element['name']}</h2>
-                <img src="${URL_PICTURE + element['name'] + ".png"}">
+         <div onclick="showModal('${name}')" id="${name}" class = "pokemon-container">
+                 <div class="pokemon-card">
+                <h2>${name}</h2>
+                <img src="${URL_PICTURE + name + ".png"}">
             </div>
          </div>
          `
         }
 
-function showModal(name) {
+
+function showModal(name) { 
+    const typeName = document.getElementById(name).classList[1]
+    const modalContent = document.getElementById('modal-content') 
+    document.getElementById('modal-content').addEventListener("click",(e) => {e.stopPropagation() })
    modal.classList.remove('hidden')
    modal.classList.add('visible')
-   updateContent(name.id);
+   modalContent.classList.add(typeName)
+   
+   updateContent(name);
 }
 
 
-function hideModal() {
+function hideModal(event) {
+    document.getElementById('modal-content').className =''
     modal.classList.remove('visible');
     modal.classList.add('hidden');
 }
@@ -69,27 +87,18 @@ function hideModal() {
 
 async function updateContent(name) {
     
-    let color = await getData(URL_COLOR)
-    
+    let color = await getData(URL_COLOR + name)
+    document.getElementById(name).classList.add(color['types'][0]['type']['name'])
     document.getElementById('modalPokemonName').innerText = name;
     document.getElementById('modalPokemonImg').src = URL_PICTURE + name + ".png";
-    document.getElementById('type').innerText = color
+    document.getElementById('type').innerText = "Type: " + color['types'][0]['type']['name']
 
 }
 
 
-// Hiermit den LoadScreeb basteln
-document.addEventListener('DOMContentLoaded' ,() =>{
-    modal.addEventListener('click',(e) => {
-        e.stopPropagation();
-    })
-})
+// Hiermit den LoadScreen basteln
 
-
-async function logoutPokemon(link) {
-    let response = await fetch(link)
-    let data = await response.json();
-
-    console.log(data['types'][0]['type']['name'])
+function init(){
+    addPokemon()
+   
 }
-logoutPokemon("https://pokeapi.co/api/v2/pokemon/pikachu/");
